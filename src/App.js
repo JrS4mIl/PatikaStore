58bb9a097b956a78e832cb320182a3a2e71e5fe9
Yesil <yesil@gmail.com>
2024-02-27 23:22:35 +0300

    feat : finish

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react'
-import {View,Text,StyleSheet,TextInput} from 'react-native';
+import {View,Text,StyleSheet,TextInput,FlatList,ScrollView} from 'react-native';
+import ProductCard from './components/ProductsCard'
+import product_data from './product_data.json'
 
 function App(){
+    const renderProduct=({item}) => <ProductCard product={item} />
+
 
     return(
-        <View>
+        <View style={styles.container}>
             <Text style={styles.text}>PATIKASTORE</Text>
             <TextInput
                     style={styles.input}
                     placeholder="Ara..."
             />
+            <View style={styles.flat}>
+            <ScrollView
+                  horizontal={true}
+                  scrollEnabled={true}
+                  showsHorizontalScrollIndicator={false}
+                >
+                <FlatList
+                    numColumns={2}
+                    data={product_data}
+                    renderItem={renderProduct}
+                            />
+            </ScrollView>
+
+            </View>
         </View>
     )
 
@@ -31,6 +49,23 @@ const styles=StyleSheet.create({
          borderRadius:12,
          backgroundColor:'#ECEFF1',
          borderColor:'white'
+    },
+    container:{
+        padding:7,
+        margin:4,
+
+
+    },
+    flat:{
+        backgroundColor:"white",
+        alignItems:"center",
+        width:'99%',
+        padding:1,
+
+
+
+
+
     }
 })
 
